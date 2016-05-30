8ce1def3020570c8a3e370261fc9c7f027202bdf
SunDwarf <sun@veriny.tf>
2016-05-30 15:07:35 +0100

    Add with_cache function for storing cached data

--- owapi/util.py
+++ owapi/util.py
@@ -3,8 +3,31 @@ Useful utilities.
 """
 import json
 
+import aioredis
 from kyokai import Request
 from kyokai.context import HTTPRequestContext
+
+
+async def with_cache(ctx: HTTPRequestContext, func, *args, expires=300):
+    """
+    Run a coroutine with cache.
+
+    Stores the result in redis.
+    """
+    assert isinstance(ctx.redis, aioredis.Redis)
+    built = func.__name__ + repr(args)
+    # Check for the key.
+    # Uses a simple func name + repr(args) as the key to use.
+    got = await ctx.redis.get(built)
+    if got:
+        return got.decode()
+
+    # Call the function.
+    result = await func(ctx, *args)
+
+    # Store the result as cached.
+    await ctx.redis.set(built, result, expire=expires)
+    return result
 
 
 def jsonify(func):
