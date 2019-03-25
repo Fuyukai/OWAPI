9591e7ae612d6db3b569f0cc60248b6096baddb2
Federico Grandi <fgrandi30@gmail.com>
2019-03-25 22:38:17 +0100

    Updated typings * Added typings for Baptsite * Minor improvements

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,149 +1,160 @@
+/* HOW TO MAINTAIN THIS FILE:
+ * every time a new hero is realeased, update HeroesPlaytime and HeroesStats by adding:
+ * heroname?: Hours (if in the first) or HeroesHeroStats (if in the second)
+ */
+
 export interface OWAPIBlob {
   _request: {
-    api_ver: Number
-    route: String
+    api_ver: number
+    route: string
   }
 
-  any: null;
-  eu: Region?;
-  kr: Region?;
-  us: Region?;
+  any: null | Region;
+  eu?: Region;
+  kr?: Region;
+  us?: Region;
 
-  msg?: String;
-  error?: Number | "Private";
+  msg?: string;
+  error?: number | 'Private';
 }
 
-interface Region {
+export interface ErrorResponse extends OWAPIBlob {
+  msg: string;
+  error: number | 'Private';
+  exc?: string
+}
+
+export interface Region {
   achievements: {
     damage: {
-      [x: string]: Boolean
-      air_strike: Boolean
-      armor_up: Boolean
-      charge: Boolean
-      clearing_the_area: Boolean
-      cold_snap: Boolean
-      cratered: Boolean
-      death_from_above: Boolean
-      did_that_sting: Boolean
-      die_die_die_die: Boolean
-      hack_the_planet: Boolean
-      huge_success: Boolean
-      ice_blocked: Boolean
-      its_high_noon: Boolean
-      mine_like_a_steel_trap: Boolean
-      power_outage: Boolean
-      raid_wipe: Boolean
-      roadkill: Boolean
-      rocket_man: Boolean
-      served_up: Boolean
-      short_fuse: Boolean
-      simple_geometry: Boolean
-      slice_and_dice: Boolean
-      smooth_as_silk: Boolean
-      special_delivery: Boolean
-      target_rich_environment: Boolean
-      the_car_wash: Boolean
-      the_dragon_is_sated: Boolean
-      their_own_worst_enemy: Boolean
-      total_recall: Boolean
-      triple_threat: Boolean
-      waste_not_want_not: Boolean
-      whoa_there: Boolean
+      [x: string]: boolean
+      air_strike: boolean
+      armor_up: boolean
+      charge: boolean
+      clearing_the_area: boolean
+      cold_snap: boolean
+      cratered: boolean
+      death_from_above: boolean
+      did_that_sting: boolean
+      die_die_die_die: boolean
+      hack_the_planet: boolean
+      huge_success: boolean
+      ice_blocked: boolean
+      its_high_noon: boolean
+      mine_like_a_steel_trap: boolean
+      power_outage: boolean
+      raid_wipe: boolean
+      roadkill: boolean
+      rocket_man: boolean
+      served_up: boolean
+      short_fuse: boolean
+      simple_geometry: boolean
+      slice_and_dice: boolean
+      smooth_as_silk: boolean
+      special_delivery: boolean
+      target_rich_environment: boolean
+      the_car_wash: boolean
+      the_dragon_is_sated: boolean
+      their_own_worst_enemy: boolean
+      total_recall: boolean
+      triple_threat: boolean
+      waste_not_want_not: boolean
+      whoa_there: boolean
     }
     general: {
-      [x: string]: Boolean
-      blackjack: Boolean
-      centenary: Boolean
-      decked_out: Boolean
-      decorated: Boolean
-      level_10: Boolean
-      level_25: Boolean
-      level_50: Boolean
-      survival_expert: Boolean
-      the_friend_zone: Boolean
-      the_path_is_closed: Boolean
-      undying: Boolean
+      [x: string]: boolean
+      blackjack: boolean
+      centenary: boolean
+      decked_out: boolean
+      decorated: boolean
+      level_10: boolean
+      level_25: boolean
+      level_50: boolean
+      survival_expert: boolean
+      the_friend_zone: boolean
+      the_path_is_closed: boolean
+      undying: boolean
     }
     maps: {
-      [x: string]: Boolean
-      cant_touch_this: Boolean
-      double_cap: Boolean
-      escort_duty: Boolean
-      lockdown: Boolean
-      shutout: Boolean
-      world_traveler: Boolean
+      [x: string]: boolean
+      cant_touch_this: boolean
+      double_cap: boolean
+      escort_duty: boolean
+      lockdown: boolean
+      shutout: boolean
+      world_traveler: boolean
     }
     special: {
-      [x: string]: Boolean
-      a_couple_of_flakes: Boolean
-      a_quiet_night: Boolean
-      ambush: Boolean
-      clean_getaway: Boolean
-      cleanup_duty: Boolean
-      cool_as_ice: Boolean
-      dawn_breaks: Boolean
-      dawn_patrol: Boolean
-      distinguished_service: Boolean
-      flagbearer: Boolean
-      four_they_were: Boolean
-      handle_with_care: Boolean
-      hardened_defenders: Boolean
-      held_the_door: Boolean
-      hot_hand: Boolean
-      into_the_shadows: Boolean
-      lucio_hat_trick: Boolean
-      mission_complete: Boolean
-      not_a_scratch: Boolean
-      plausible_deniability: Boolean
-      rampage: Boolean
-      redacted: Boolean
-      replacements: Boolean
-      safe_hands: Boolean
-      six_they_were: Boolean
-      six_wanderers: Boolean
-      snowed_in: Boolean
-      strike_team: Boolean
-      survived_the_night: Boolean
-      survivor: Boolean
-      the_venice_incident: Boolean
-      thinking_with_your_stomach: Boolean
-      unit_commendation: Boolean
-      unscathed: Boolean
-      volley: Boolean
-      whap: Boolean
-      yeti_catcher: Boolean
+      [x: string]: boolean
+      a_couple_of_flakes: boolean
+      a_quiet_night: boolean
+      ambush: boolean
+      clean_getaway: boolean
+      cleanup_duty: boolean
+      cool_as_ice: boolean
+      dawn_breaks: boolean
+      dawn_patrol: boolean
+      distinguished_service: boolean
+      flagbearer: boolean
+      four_they_were: boolean
+      handle_with_care: boolean
+      hardened_defenders: boolean
+      held_the_door: boolean
+      hot_hand: boolean
+      into_the_shadows: boolean
+      lucio_hat_trick: boolean
+      mission_complete: boolean
+      not_a_scratch: boolean
+      plausible_deniability: boolean
+      rampage: boolean
+      redacted: boolean
+      replacements: boolean
+      safe_hands: boolean
+      six_they_were: boolean
+      six_wanderers: boolean
+      snowed_in: boolean
+      strike_team: boolean
+      survived_the_night: boolean
+      survivor: boolean
+      the_venice_incident: boolean
+      thinking_with_your_stomach: boolean
+      unit_commendation: boolean
+      unscathed: boolean
+      volley: boolean
+      whap: boolean
+      yeti_catcher: boolean
     }
     support: {
-      [x: string]: Boolean
-      antipode: Boolean
-      enabler: Boolean
-      excuse_me: Boolean
-      grounded: Boolean
-      group_health_plan: Boolean
-      huge_rez: Boolean
-      naptime: Boolean
-      rapid_discord: Boolean
-      simple_trigonometry: Boolean
-      supersonic: Boolean
-      the_floor_is_lava: Boolean
-      the_iris_embraces_you: Boolean
+      [x: string]: boolean
+      antipode: boolean
+      enabler: boolean
+      excuse_me: boolean
+      grounded: boolean
+      group_health_plan: boolean
+      huge_rez: boolean
+      naptime: boolean
+      rapid_discord: boolean
+      simple_trigonometry: boolean
+      supersonic: boolean
+      the_floor_is_lava: boolean
+      the_iris_embraces_you: boolean
     }
     tank: {
-      [x: string]: Boolean
-      adaptation: Boolean
-      anger_management: Boolean
-      game_over: Boolean
-      giving_you_the_hook: Boolean
-      halt_state: Boolean
-      hog_wild: Boolean
-      i_am_your_shield: Boolean
-      mine_sweeper: Boolean
-      overclocked: Boolean
-      power_overwhelming: Boolean
-      shot_down: Boolean
-      storm_earth_and_fire: Boolean
-      strike: Boolean
-      the_power_of_attraction: Boolean
+      [x: string]: boolean
+      adaptation: boolean
+      anger_management: boolean
+      game_over: boolean
+      giving_you_the_hook: boolean
+      halt_state: boolean
+      hog_wild: boolean
+      i_am_your_shield: boolean
+      mine_sweeper: boolean
+      overclocked: boolean
+      power_overwhelming: boolean
+      shot_down: boolean
+      storm_earth_and_fire: boolean
+      strike: boolean
+      the_power_of_attraction: boolean
     }
   }
 
@@ -166,6 +177,7 @@ interface Region {
 interface Hours extends Number { }
 interface HeroesPlaytime {
   ana?: Hours
+  baptiste?: Hours
   bastion?: Hours
   brigitte?: Hours
   dva?: Hours
@@ -193,6 +205,7 @@ interface HeroesPlaytime {
 }
 interface HeroesStats {
   ana?: HeroesHeroStats
+  baptiste?: HeroesHeroStats
   bastion?: HeroesHeroStats
   brigitte?: HeroesHeroStats
   dva?: HeroesHeroStats
@@ -220,142 +233,142 @@ interface HeroesStats {
 }
 interface HeroesHeroStats {
   general_stats: {
-    [x: String]: Number
-    all_damage_done: Number
-    all_damage_done_most_in_game: Number
-    all_damage_done_most_in_life: Number
-    barrier_damage_done: Number
-    barrier_damage_done_most_in_game: Number
-    blaster_kills: Number
-    blaster_kills_most_in_game: Number
-    cards: Number
-    critical_hit_accuracy: Number
-    critical_hits: Number
-    critical_hits_most_in_game: Number
-    critical_hits_most_in_life: Number
-    damage_amplified: Number
-    damage_amplified_most_in_game: Number
-    deaths: Number
-    defensive_assists: Number
-    defensive_assists_most_in_game: Number
-    eliminations: Number
-    eliminations_most_in_game: Number
-    eliminations_most_in_life: Number
-    eliminations_per_life: Number
-    final_blows: Number
-    final_blows_most_in_game: Number
-    games_lost: Number
-    games_played: Number
-    games_won: Number
-    healing_done: Number
-    healing_done_most_in_game: Number
-    hero_damage_done: Number
-    hero_damage_done_most_in_game: Number
-    hero_damage_done_most_in_life: Number
-    kill_streak_best: Number
-    medals: Number
-    medals_bronze: Number
-    medals_gold: Number
-    medals_silver: Number
-    melee_final_blow: Number
-    melee_final_blow_most_in_game: Number
-    objective_kills: Number
-    objective_kills_most_in_game: Number
-    objective_time: Number
-    objective_time_most_in_game: Number
-    offensive_assists: Number
-    offensive_assists_most_in_game: Number
-    players_resurrected: Number
-    players_resurrected_most_in_game: Number
-    secondary_fire_accuracy: Number
-    self_healing: Number
-    self_healing_most_in_game: Number
-    time_played: Number
-    time_spent_on_fire: Number
-    time_spent_on_fire_most_in_game: Number
-    weapon_accuracy: Number
-    weapon_accuracy_best_in_game: Number
-    win_percentage: Number
+    [x: string]: number
+    all_damage_done: number
+    all_damage_done_most_in_game: number
+    all_damage_done_most_in_life: number
+    barrier_damage_done: number
+    barrier_damage_done_most_in_game: number
+    blaster_kills: number
+    blaster_kills_most_in_game: number
+    cards: number
+    critical_hit_accuracy: number
+    critical_hits: number
+    critical_hits_most_in_game: number
+    critical_hits_most_in_life: number
+    damage_amplified: number
+    damage_amplified_most_in_game: number
+    deaths: number
+    defensive_assists: number
+    defensive_assists_most_in_game: number
+    eliminations: number
+    eliminations_most_in_game: number
+    eliminations_most_in_life: number
+    eliminations_per_life: number
+    final_blows: number
+    final_blows_most_in_game: number
+    games_lost: number
+    games_played: number
+    games_won: number
+    healing_done: number
+    healing_done_most_in_game: number
+    hero_damage_done: number
+    hero_damage_done_most_in_game: number
+    hero_damage_done_most_in_life: number
+    kill_streak_best: number
+    medals: number
+    medals_bronze: number
+    medals_gold: number
+    medals_silver: number
+    melee_final_blow: number
+    melee_final_blow_most_in_game: number
+    objective_kills: number
+    objective_kills_most_in_game: number
+    objective_time: number
+    objective_time_most_in_game: number
+    offensive_assists: number
+    offensive_assists_most_in_game: number
+    players_resurrected: number
+    players_resurrected_most_in_game: number
+    secondary_fire_accuracy: number
+    self_healing: number
+    self_healing_most_in_game: number
+    time_played: number
+    time_spent_on_fire: number
+    time_spent_on_fire_most_in_game: number
+    weapon_accuracy: number
+    weapon_accuracy_best_in_game: number
+    win_percentage: number
   }
   rolling_average_stats: {
-    [x: String]: Number
-    all_damage_done: Number
-    barrier_damage_done: Number
-    blaster_kills: Number
-    critical_hits: Number
-    damage_amplified: Number
-    deaths: Number
-    defensive_assists: Number
-    eliminations: Number
-    final_blows: Number
-    healing_done: Number
-    hero_damage_done: Number
-    melee_final_blows: Number
-    objective_kills: Number
-    objective_time: Number
-    offensive_assists: Number
-    players_resurrected: Number
-    self_healing: Number
-    time_spent_on_fire: Number
+    [x: string]: number
+    all_damage_done: number
+    barrier_damage_done: number
+    blaster_kills: number
+    critical_hits: number
+    damage_amplified: number
+    deaths: number
+    defensive_assists: number
+    eliminations: number
+    final_blows: number
+    healing_done: number
+    hero_damage_done: number
+    melee_final_blows: number
+    objective_kills: number
+    objective_time: number
+    offensive_assists: number
+    players_resurrected: number
+    self_healing: number
+    time_spent_on_fire: number
   }
 }
 interface Stats {
-  competitive: Boolean
+  competitive: boolean
   game_stats: {
-    all_damage_done: Number
-    all_damage_done_most_in_game: Number
-    barrier_damage_done: Number
-    barrier_damage_done_most_in_game: Number
-    cards: Number
-    damage_done: Number
-    deaths: Number
-    defensive_assists: Number
-    defensive_assists_most_in_game: Number
-    eliminations: Number
-    eliminations_most_in_game: Number
-    final_blows: Number
-    final_blows_most_in_game: Number
-    games_lost: Number
-    games_played: Number
-    games_won: Number
-    healing_done: Number
-    healing_done_most_in_game: Number
-    hero_damage_done: Number
-    hero_damage_done_most_in_game: Number
-    kill_streak_best: Number
-    kpd: Number
-    medals: Number
-    medals_bronze: Number
-    medals_gold: Number
-    medals_silver: Number
-    melee_final_blow: Number
-    melee_final_blow_most_in_game: Number
-    objective_kills: Number
-    objective_kills_most_in_game: Number
-    objective_time: Number
-    objective_time_most_in_game: Number
-    offensive_assists: Number
-    offensive_assists_most_in_game: Number
-    solo_kills_most_in_game: Number
-    time_played: Number
-    time_spent_on_fire: Number
-    time_spent_on_fire_most_in_game: Number
+    all_damage_done: number
+    all_damage_done_most_in_game: number
+    barrier_damage_done: number
+    barrier_damage_done_most_in_game: number
+    cards: number
+    damage_done: number
+    deaths: number
+    defensive_assists: number
+    defensive_assists_most_in_game: number
+    eliminations: number
+    eliminations_most_in_game: number
+    final_blows: number
+    final_blows_most_in_game: number
+    games_lost: number
+    games_played: number
+    games_won: number
+    healing_done: number
+    healing_done_most_in_game: number
+    hero_damage_done: number
+    hero_damage_done_most_in_game: number
+    kill_streak_best: number
+    kpd: number
+    medals: number
+    medals_bronze: number
+    medals_gold: number
+    medals_silver: number
+    melee_final_blow: number
+    melee_final_blow_most_in_game: number
+    objective_kills: number
+    objective_kills_most_in_game: number
+    objective_time: number
+    objective_time_most_in_game: number
+    offensive_assists: number
+    offensive_assists_most_in_game: number
+    solo_kills_most_in_game: number
+    time_played: number
+    time_spent_on_fire: number
+    time_spent_on_fire_most_in_game: number
   }
   overall_stats: {
-    avatar: URL
-    comprank: Number?;
-    endorsement_level: Number
-    endorsement_shotcaller: Number
-    endorsement_sportsmanship: Number
-    endorsement_teammate: Number
-    games: Number
-    level: Number
-    losses: Number
-    prestige: Number?;
-    tier: Number?;
-    ties?: Number
-    win_rate: Number
-    wins: Number
+    avatar: string
+    comprank?: number;
+    endorsement_level: number
+    endorsement_shotcaller: number
+    endorsement_sportsmanship: number
+    endorsement_teammate: number
+    games: number
+    level: number
+    losses: number
+    prestige?: number;
+    tier?: number;
+    ties?: number
+    win_rate: number
+    wins: number
   }
 }
 interface StatsComp extends Stats {
